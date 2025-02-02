ea11d48eae4a81332c09e98b9bd8285a46dae516
dotmatrix <youremail@yourdomain.com>
2025-02-02 14:58:18 +0100

    Convert info message types to uppercase in IRCBot class

--- classes.py
+++ classes.py
@@ -361,6 +361,10 @@ class IRCBot(irc.bot.SingleServerIRCBot):
         #print(result)
         if not result or not result["release"] or not result["type"]:
             return False
+
+        # Convert type to uppercase
+        result["type"] = result["type"].upper()
+        
         try:
             self.lock.acquire()
             cursor = self.conn.cursor()
